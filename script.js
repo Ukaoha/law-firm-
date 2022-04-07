fd9a93a379605deee6fe88ac5d006482ddb4a5dd
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-04-07 22:28:29 +0100

    login and signup

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function closeMenu() {
   navItem.classList.remove('active')
 }
 
+// login page icon
+function eyeIcon() {
+  let eyePassword = document.getElementById('login_password');
+  let hidePassword1 = document.getElementById('hide1');
+  let hidePassword2 = document.getElementById('hide2');
+  
+  if(eyePassword.type === 'password'){
+    eyePassword.type = "text";
+    hidePassword1.style.display = 'inline-block';
+    hidePassword2.style.display = 'none';
+  } else {
+    eyePassword.type = "password";
+    hidePassword1.style.display = 'none';
+    hidePassword2.style.display = 'inline-block';
+
+  }
+}
